f376dd36030830852300e52ed3f24d0e290c85dc
JumpingPhantom <104843253+jumpingphantom@users.noreply.github.com>
2024-02-28 00:14:36 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'border-radius-previewer';
+  topLeft: number = 0;
+  topRight: number = 0;
+  bottomLeft: number = 0;
+  bottomRight: number = 0;
+  radius: string = '0';
+
+  handleInputChange(event: any) {
+    const classType = event.target.classList[1];
+    switch (classType) {
+      case 'top-left':
+        this.topLeft = event.target.value;
+        break;
+      case 'top-right':
+        this.topRight = event.target.value;
+        break;
+      case 'bottom-left':
+        this.bottomLeft = event.target.value;
+        break;
+      case 'bottom-right':
+        this.bottomRight = event.target.value;
+        break;
+    }
+    this.radius = `${this.topLeft}px ${this.topRight}px ${this.bottomRight}px ${this.bottomLeft}px`;
+  }
 }
